dbddb2d1e4352be7d2403ea3b9872f8a1491856b
reyvanomario <reyvanomario24122004@gmail.com>
2024-11-13 15:29:08 +0700

    menyelesaikan tutorial 8

--- mental_health_tracker/lib/widgets/left_drawer.dart
+++ mental_health_tracker/lib/widgets/left_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_tracker/screens/list_moodentry.dart';
 import 'package:mental_health_tracker/screens/menu.dart';
 import 'package:mental_health_tracker/screens/moodentry_form.dart';
 
@@ -56,10 +57,6 @@ class LeftDrawer extends StatelessWidget {
               title: const Text('Tambah Mood'),
               // Bagian redirection ke MoodEntryFormPage
               onTap: () {
-                /*
-                TODO: Buatlah routing ke MoodEntryFormPage di sini,
-                setelah halaman MoodEntryFormPage sudah dibuat.
-                */
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -67,6 +64,17 @@ class LeftDrawer extends StatelessWidget {
                   ));
               },
             ),
+            ListTile(
+              leading: const Icon(Icons.add_reaction_rounded),
+              title: const Text('Daftar Mood'),
+              onTap: () {
+                  // Route menu ke halaman mood
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MoodEntryPage()),
+                  );
+              },
+          ),
         ],
       ),
     );
